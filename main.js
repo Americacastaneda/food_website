29d3029503699cebdfba0e0039496ef361a2653b
Americacastaneda <castanedaamerica5@gmail.com>
2019-07-12 10:23:11 -0700

    init commit

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log(";^;");
+let response = prompt("What's your name?")
+//alert("FBI!!!")
+
+let greet = document.getElementById('greet')
+.innerHTML = 'wattup ' + response
+
+console.log(response)
+
+document.getElementById("demo").innerHTML = "Hey JavaScript";
